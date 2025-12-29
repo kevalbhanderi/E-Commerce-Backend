9a2ea633e6552bdb9463c1155531db66ee720a70
keval <kbhanderi@solinkcorp.com>
2025-12-29 22:11:26 +0530

    fix: linting error

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [
-        MongooseModule.forRoot(`mongodb://${process.env.MONGO_URL}`, {
-            auth: { username: process.env.MONGO_USER, password: process.env.MONGO_PASS },
-            dbName: process.env.MONGO_DB,
-            authSource: process.env.MONGO_AUTH_DB,
-        }),
-    ]
+  imports: [
+    MongooseModule.forRoot(`mongodb://${process.env.MONGO_URL}`, {
+      auth: {
+        username: process.env.MONGO_USER,
+        password: process.env.MONGO_PASS,
+      },
+      dbName: process.env.MONGO_DB,
+      authSource: process.env.MONGO_AUTH_DB,
+    }),
+  ],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
